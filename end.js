2dfc2c91585689840f6b74ef66cf9e0bc8a511c5
Anshul Shanker <anshulshankar@gmail.com>
2020-12-18 01:35:33 -0600

    Add highscores page

--- end.js
+++ end.js
@@ -2,6 +2,9 @@ const username = document.getElementById('username');
 const saveScoreButton = document.getElementById('saveScore');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
@@ -13,4 +16,18 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = e => {
   e.preventDefault();
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value
+  };
+
+  highScores.push(score);
+  
+  // Sort array and keep only top 5 scores
+  highScores.sort((a,b) => b.score - a.score)
+  highScores.splice(5);
+
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+  window.location.assign("/");
 }
