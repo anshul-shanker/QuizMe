addada430e2fbaf9f1de39024362bb10e1ac52f3
Anshul Shanker <anshulshankar@gmail.com>
2020-12-18 04:27:17 -0600

    Fix go gome btn

--- game.js
+++ game.js
@@ -36,7 +36,7 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&type=multiple")
     // JSON.parse(.replace(/&quot;/g,'"'));
     const answerChoices = [...loadedQuestion.incorrect_answers];
     formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
-    answerChoices.splice(formattedQuestion.answer -1,0, loadedQuestion.correct_answer);
+    answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer);
 
     answerChoices.forEach((choice, index) => {
       formattedQuestion["choice" + (index + 1)] = choice;
